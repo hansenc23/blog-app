675990ae17dec52f6fb838bf8eb03fcc63ee32f9
hansenc23 <hansenc216@gmail.com>
2020-08-03 21:30:29 +1000

    first commit

---
+++ src/build/precache-manifest.f8599b4a8932ddd104418a4abc09ebdc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e5a403cec228b1918feb708f2aa0c38c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b2d88f804b453e0a9fb7",
+    "url": "/static/css/main.bbd23df2.chunk.css"
+  },
+  {
+    "revision": "76f38579096529c76309",
+    "url": "/static/js/2.a579bb34.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.a579bb34.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b2d88f804b453e0a9fb7",
+    "url": "/static/js/main.42c7367e.chunk.js"
+  },
+  {
+    "revision": "caf43f0a14314da69f12",
+    "url": "/static/js/runtime-main.226f7c93.js"
+  }
+]);
